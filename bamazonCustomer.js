a9dcf0956999121f3f3b29f4b98d7a52f8202825
James Davis <james.jpdavis@gmail.com>
2016-06-26 13:36:38 -0400

    initial bamazon customer working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,1 +1,21 @@
 var mysql = require('mysql');
+
+var con = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'root',
+	database: 'bamazon',
+	port: 8889
+});
+
+var showAll = function(){
+con.query('SELECT * FROM products', function(err, list){
+  if (err){
+    throw (err);
+  }else{
+    console.log(list);
+    }
+  });
+};
+
+showAll();
