e8e1b59f26270e3c22ec9c05e7321b0fc243d694
James Davis <james.jpdavis@gmail.com>
2016-06-26 14:44:48 -0400

    removed duplicat file bamazon, everything now in bamazonCustomer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,6 @@
-var mysql = require('mysql');
+var mysql= require('mysql');
+var prompt= require('prompt');
+
 
 var con = mysql.createConnection({
 	host: 'localhost',
@@ -8,14 +10,63 @@ var con = mysql.createConnection({
 	port: 8889
 });
 
-var showAll = function(){
-con.query('SELECT * FROM products', function(err, list){
-  if (err){
-    throw (err);
-  }else{
-    console.log(list);
-    }
-  });
-};
+con.connect(function (err){
+	if (err){
+		callback(err);
+	}
+	console.log('connected');
+});
 
-showAll();
+con.query('Select ItemID,ProductName,DepartmentName,Price FROM products',function(err,result){
+	if (err){
+		throw (err);
+	}else{
+		console.log('Welcome to Bamazon, home of the Bamazon, the greatest fictional sporting goods store of all time');
+		var results= result;
+		for (var i = 0; i < results.length; i++) {
+
+			console.log('We have: ' + results[i].ProductName + ' in ' + results[i].DepartmentName + ' for $' + results[i].Price);
+		}
+	}
+
+
+var order= function(){
+ 	console.log('Please enter what you would like to purchase.');//if you have more items than...
+ 		prompt.start();
+ 		prompt.get(['ProductName','quantity'], function(err, result){
+ 			if(err){
+ 				throw (err);
+ 			}else{
+ 				con.query('SELECT * FROM products', function(err, list){
+ 					if (err){
+ 						throw (err);
+ 					}else{
+ 						for (var i = 0; i < list.length; i++) {
+ 							if(list[i].StockQuantity > result.quantity && list[i].ProductName === result.ProductName){
+ 								console.log('You owe: $' + result.quantity * list[i].Price);
+
+
+
+ 								break;
+
+ 							}else if( list[i].StockQuantity < result.quantity){
+ 								console.log('This request exceeds stock capacity! Try again.');
+ 								order();
+ 								break;
+
+ 								//var newQuant= function(){
+ 								//	con.query('UPDATE products SET StockQuantity= ' + listStockQuantity)
+ 								}
+
+ 						    }
+ 						}
+
+ 				})
+ 			}
+ 			//console.log('You ordered: ' + result.quantity + ' ' + result.ProductName);
+
+ 		});
+ 	 	}
+ 	 	order();
+
+ 	 });
