c03210dd944e6e99fbb4c56a4917bd2985e54e03
James Davis <james.jpdavis@gmail.com>
2016-08-08 19:34:27 -0400

    connection and console statement

--- bamazonManager.js
+++ bamazonManager.js
@@ -17,3 +17,25 @@ con.connect(function (err){
 	}
 	console.log('connected');
 });
+
+
+console.log('Please select from the following by typing 1,2,3 or 4.\n 1) View Products for Sale\n 2) View Low Inventory\n 3) Add to Inventory\n 4) Add New Product');
+// switch(input[0]){
+//     case "my-tweets"://if input[0] = my-tweets call twitter() function
+//         twitter();
+//         break;
+//     case "do-what-it-says":
+//         noEntry();
+//         break;
+//     case "spotify-this-song":
+//         	spotify(input[1]);
+//         break;
+//     case "movie-this":
+//         if(input[1]){
+//         	request();
+//         }else{
+//         	input[1]= "Mr. Nobdy";
+//         	request();
+//         }
+//         break;
+// }
